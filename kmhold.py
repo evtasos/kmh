e10202d13ce9090dd72ab7fed350eed222f04350
evtasos <evtasos1@gmail.com>
2023-07-06 19:52:08 +0300

    adding bootstrap slowly

--- kmhold.py
+++ kmhold.py
@@ -9,9 +9,13 @@ patient_counter = 1
 # Placeholder lists to store patient data
 issued_numbers = []
 office_pool = []
-assigned_offices = {'Γραφείο1': [], 'Γραφείο2': [],'Αιματολογικό': [],}
-treatment_waiting_pool = []
-treatment_rooms = {1: [], 2: [], 3: [],4: [], 5: [], 6: [], 7: [], 8: []}
+chemo_waiting_pool = []
+assigned_offices = {}
+treatment_rooms = {
+    1: [],
+    2: [],
+    3: []
+}
 
 @app.route('/issue-number')
 def issue_number():
@@ -28,57 +32,77 @@ def assign_office():
     office_number = request.json['office_number']
     if barcode in office_pool:
         office_pool.remove(barcode)
-        assigned_offices.setdefault(office_number, []).append(barcode)
+        if office_number not in assigned_offices:
+            assigned_offices[office_number] = []
+        assigned_offices[office_number].append(barcode)
+        # Perform necessary actions with the assigned office number
         return jsonify({'message': f'Patient with barcode {barcode} assigned to office {office_number}.'})
-    return jsonify({'message': 'Barcode not found in the office pool.'}), 404
+    else:
+        return jsonify({'message': 'Barcode not found in the office pool.'}), 404
 
 @app.route('/add-to-chemo-waiting', methods=['POST'])
 def add_to_chemo_waiting():
     barcode = request.json['barcode']
     eligible = request.json['status']
+    
+    if any(barcode in value for value in assigned_offices.values()):
+        office_number = None
+        for key, value in assigned_offices.items():
+            if barcode in value:
+                office_number = key
+                break
 
-    for office_number, barcodes in assigned_offices.items():
-        if barcode in barcodes:
-            barcodes.remove(barcode)
-            if eligible:
-                treatment_waiting_pool.append(barcode)
-                return jsonify({'message': f'Patient with barcode {barcode} added to chemo waiting pool. Removed from office {office_number}.'})
-            else:
-                treatment_waiting_pool.remove(barcode)
-                return jsonify({'message': f'Patient with barcode {barcode} is not eligible for chemo. Removed from office {office_number}.'})
+        assigned_offices[office_number].remove(barcode)
+        if eligible:
+            chemo_waiting_pool.append(barcode)  # Add the barcode to the chemo waiting pool
+            return jsonify({'message': f'Patient with barcode {barcode} added to chemo waiting pool. Removed from office {office_number}.'})
+        else:
+            if barcode in chemo_waiting_pool:
+                chemo_waiting_pool.remove(barcode)  # Remove the barcode from the chemo waiting pool
+            return jsonify({'message': f'Patient with barcode {barcode} is not eligible for chemo. Removed from office {office_number}.'})
+    else:
+        return jsonify({'message': 'Barcode not found in the assigned offices.'}), 404
 
-    return jsonify({'message': 'Barcode not found in the assigned offices.'}), 404
+
+
 
 @app.route('/assign-treatment-room', methods=['POST'])
 def assign_treatment_room():
     room_number = int(request.json['room_number'])
     barcode = request.json['barcode']
 
-    if barcode in treatment_waiting_pool:
-        treatment_rooms[room_number].append(barcode)
-        treatment_waiting_pool.remove(barcode)
-        return jsonify({'barcode': barcode, 'message': f'Patient with barcode {barcode} assigned to treatment room {room_number}.'})
-    return jsonify({'message': 'Barcode not found in the Chemo Waiting Pool.'}), 404
-
+    if chemo_waiting_pool:
+        if barcode in chemo_waiting_pool:
+            treatment_rooms[room_number].append(barcode)
+            chemo_waiting_pool.remove(barcode)
+            return jsonify({'barcode': barcode, 'message': f'Patient with barcode {barcode} assigned to treatment room {room_number}.'})
+        else:
+            return jsonify({'message': 'Barcode not found in the Chemo Waiting Pool.'}), 404
+    else:
+        return jsonify({'message': 'No barcodes available in the Chemo Waiting Pool.'})
+    
 @app.route('/rmv-barcode', methods=['POST'])
-def remove_barcode():
+def rmv_barcode():
     barcode = request.json['barcode']
-
+    
     if barcode in office_pool:
         office_pool.remove(barcode)
         return jsonify({'barcode': barcode, 'message': f'Patient with barcode {barcode} left office'})
-    for office_number, barcodes in assigned_offices.items():
-        if barcode in barcodes:
-            barcodes.remove(barcode)
+    for office in assigned_offices.values():
+        if barcode in office:
+            office.remove(barcode)
             return jsonify({'barcode': barcode, 'message': f'Patient with barcode {barcode} left offices'})
-    for room_number, barcodes in treatment_rooms.items():
-        if barcode in barcodes:
-            barcodes.remove(barcode)
+    
+    for room in treatment_rooms.values():
+        if barcode in room:
+            room.remove(barcode)
             return jsonify({'barcode': barcode, 'message': f'Patient with barcode {barcode} left treatment'})
-    if barcode in treatment_waiting_pool:
-        treatment_waiting_pool.remove(barcode)
+    
+    if barcode in chemo_waiting_pool:
+        chemo_waiting_pool.remove(barcode)
         return jsonify({'barcode': barcode, 'message': f'Patient with barcode {barcode} left chemo pool'})
-    return jsonify({'message': 'Barcode not found '}), 404
+    else:
+        return jsonify({'message': 'Barcode not found '}), 404 
 
 @app.route('/office-pool')
 def get_office_pool():
@@ -90,7 +114,7 @@ def get_assigned_offices():
 
 @app.route('/chemo-waiting-pool')
 def get_chemo_waiting_pool():
-    return jsonify({'chemo_waiting_pool': treatment_waiting_pool})
+    return jsonify({'chemo_waiting_pool': chemo_waiting_pool})
 
 @app.route('/treatment-rooms')
 def get_treatment_rooms():
@@ -100,14 +124,14 @@ def get_treatment_rooms():
 def view_pools():
     office_numbers = sorted(assigned_offices.keys())
     return render_template('pools.html', office_pool=office_pool, assigned_offices=assigned_offices,
-                           chemo_waiting_pool=treatment_waiting_pool, treatment_rooms=treatment_rooms,
+                           chemo_waiting_pool=chemo_waiting_pool, treatment_rooms=treatment_rooms,
                            office_numbers=office_numbers)
 
 @app.route('/pools2')
 def view_pools2():
     office_numbers = sorted(assigned_offices.keys())
     return render_template('poolsbare.html', office_pool=office_pool, assigned_offices=assigned_offices,
-                           chemo_waiting_pool=treatment_waiting_pool, treatment_rooms=treatment_rooms,
+                           chemo_waiting_pool=chemo_waiting_pool, treatment_rooms=treatment_rooms,
                            office_numbers=office_numbers)
 
 @app.route('/numbers')
@@ -120,9 +144,14 @@ def reset():
     patient_counter = 1
     issued_numbers.clear()
     office_pool.clear()
+    chemo_waiting_pool.clear()
     assigned_offices.clear()
-    treatment_waiting_pool.clear()
-    treatment_rooms = {1: [], 2: [], 3: []}
+    treatment_rooms.clear()
+    treatment_rooms.update({
+        1: [],
+        2: [],
+        3: []
+    })
     return jsonify({'message': 'System reset successfully.'})
 
 if __name__ == '__main__':
